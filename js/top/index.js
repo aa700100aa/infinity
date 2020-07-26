548c1da405253d3ce1f6e8e057a3a0ed8516237f
aa700100aa <aa700100aa@gmail.com>
2020-07-26 15:23:07 +0900

    feature/kadai2 js 修正

--- js/top/index.js
+++ js/top/index.js
@@ -1,19 +1,17 @@
 window.$ = require('jquery');
 var slick = require('slick-carousel');
 
-/*---------------------------------------------
-  jQuery
------------------------------------------------*/
-$(function () {
+((d, w) => {
 
+  //jQuery
   /*---------------------------------------------
     contentsLink スライダー
   -----------------------------------------------*/
   // ブレイクポイント定義
-  var tabletWidth = window.matchMedia( '(min-width:750px) and (max-width:993px)' );
-  var mobileWidth = window.matchMedia('screen and (max-width: 749px)');
+  const tabletWidth = w.matchMedia('(min-width:750px) and (max-width:993px)');
+  const mobileWidth = w.matchMedia('screen and (max-width: 749px)');
 
-  function checkBreakPoint() {
+  checkBreakPoint = () => {
     if (tabletWidth.matches) {
       //タブレット
       $('.js-contentsLinkSlider').slick({
@@ -22,9 +20,9 @@ $(function () {
         speed: 150,
         arrows: false,
         infinite: false,
-        slidesToShow: document.body.clientWidth / 310
+        slidesToShow: d.body.clientWidth / 310
       });
-      $('.js-contentsLinkSlider').on('setPosition', function () {
+      $('.js-contentsLinkSlider').on('setPosition', () => {
         //インラインスタイル オーバーライド
         $('.slick-slide').first().css('width', '11.3%');
       });
@@ -36,9 +34,9 @@ $(function () {
         speed: 275,
         arrows: false,
         infinite: false,
-        slidesToShow: document.body.clientWidth / 210
+        slidesToShow: d.body.clientWidth / 210
       });
-      $('.js-contentsLinkSlider').on('setPosition', function () {
+      $('.js-contentsLinkSlider').on('setPosition', () => {
         //インラインスタイル オーバーライド
         $('.slick-slide').first().css('width', '50px');
       });
@@ -69,161 +67,153 @@ $(function () {
     infinite: false,
     swipe: true
   });
-  
-  function textSlideIn() {
-    $('.slick-slide').each(function (index, element) {
+
+  textSlideIn = () => {
+    $('.slick-slide').each((index,element) => {
       if ($(element).hasClass('slick-active')) {
         $(element).find('.js-videoTitle').addClass('add-slideIn');
         $(element).find('.js-vieoTime').addClass('add-slideIn');
       } else {
-        setTimeout(function () {
+        setTimeout(() => {
           $(element).find('.js-videoTitle').removeClass('add-slideIn');
           $(element).find('.js-vieoTime').removeClass('add-slideIn');
         }, 150);
       }
     })
   }
-  $('.js-videoSlider').on('swipe', function () {
-    textSlideIn();
-  });
-  $('.js-slickArrow').click(function() {
-    textSlideIn();
-  })
+  $('.js-videoSlider').on('click swipe', textSlideIn);
 
-});
-
-/*---------------------------------------------
-  JavaScript（Vanilla JS）
------------------------------------------------*/
-document.addEventListener('DOMContentLoaded', () => {
-
+  //JavaScript（Vanilla JS）
   /*---------------------------------------------
-    キーボードショートカット　コピー禁止
+    右クリック&キーボードショートカット 禁止
   -----------------------------------------------*/
-  document.body.oncopy = () => { return false;}
+  d.oncontextmenu = () => { return false; }
 
-  /*---------------------------------------------
-    header 文字&ボーダー 表示非表示 切り替え
-  -----------------------------------------------*/
-  const headerText = document.querySelectorAll('.js-headerText');
-  const headerborder = document.querySelectorAll('.js-headerBorder');
-  let count = 0;
-  //ヘッダーの文字切り替えインターバル(cssで定義した値と同値) 
-  const headerActionTime = 7000;
-  setInterval(() => {
-    //文字 切り替え
-    headerText[count].classList.remove('add-active');
-    count = count === headerText.length - 1 ? 0 : count += 1;//最後の要素を表示したらリセット
-    headerText[count].classList.add('add-active');
-    //ボーダー 表示非表示 切り替え
-    if (count === 0) {
-      headerborder[0].classList.remove('add-display');
-      headerborder[1].classList.remove('add-display');
-    } else {
-      headerborder[0].classList.add('add-display');
-      headerborder[1].classList.add('add-display');
+  d.addEventListener('keydown',function(){
+    //"b"（keyCode=66）が押された場合は入力を無効にする
+    if(event.keyCode === 73){
+      event.preventDefault();
     }
-  }, headerActionTime);
-  
-  /*---------------------------------------------
-    オープニングアニメーション
-  -----------------------------------------------*/
-  //オープニングアニメーションにかかる時間(cssで定義した値と同値) 
-  const opAnimationTime = 2500;
-  const opAnimationTrigger = document.querySelectorAll('.js-trigger');
-  window.addEventListener('load', () => {
+  });
+
+  w.addEventListener('load', () => {
+    /*---------------------------------------------
+      header 文字&ボーダー 表示非表示 切り替え
+    -----------------------------------------------*/
+    const headerText = d.querySelectorAll('.js-headerText');
+    const headerborder = d.querySelectorAll('.js-headerBorder');
+    let count = 0;
+    //ヘッダーの文字切り替えインターバル(cssで定義した値と同値) 
+    const headerActionTime = 7000;
+    headerTextChange = () => {
+      //文字 切り替え
+      headerText[count].classList.remove('add-active');
+      count = count === headerText.length - 1 ? 0 : count += 1;//最後の要素を表示したらリセット
+      headerText[count].classList.add('add-active');
+      //ボーダー 表示非表示 切り替え
+      headerborder[0].classList[count ? 'add' : 'remove']('add-display');
+      headerborder[1].classList[count ? 'add' : 'remove']('add-display');
+    }
+    setInterval(headerTextChange, headerActionTime);
+
+    /*---------------------------------------------
+      オープニングアニメーション
+    -----------------------------------------------*/
+    //オープニングアニメーションにかかる時間(cssで定義した値と同値) 
+    const opAnimationTime = 2500;
+    const opAnimationTrigger = d.querySelectorAll('.js-trigger');
     //アニメーション開始
     for (let i = 0; i < opAnimationTrigger.length; i++) {
       opAnimationTrigger[i].classList.add('add-trigger');
     }
-
     //アニメーション終了
     setTimeout(() => {
-      document.querySelector('.js-opAnimation').classList.add('add-displayNone');
+      d.getElementById('js-opAnimation').classList.add('add-displayNone');
       //アニメーション終了時、常にページの一番を指定
       scrollTo(0, 0);
       //ページ再読み込み時にもスクロールエフェクトを発生させるためクラスを外す
-      for (let j = 0; j < effectElement.length; j++) {
-        effectElement[j].classList.remove('add-inView');
+      for (let i = 0; i < effectElement.length; i++) {
+        effectElement[i].classList.remove('add-inView');
       }
     }, opAnimationTime * 0.48);
     setTimeout(() => {
-      document.querySelector('.js-heroOverlayPoster').classList.add('add-displayNone');
-      document.querySelector('.js-heroOverlayGrids').classList.add('add-displayNone');
+      d.getElementById('js-heroOverlayPoster').classList.add('add-displayNone');
+      d.getElementById('js-heroOverlayGrids').classList.add('add-displayNone');
     }, opAnimationTime);
+
+    /*---------------------------------------------
+      スクロールエフェクト
+    -----------------------------------------------*/
+    const effectElement = d.querySelectorAll('.js-inView');
+    let scrollPosition; //現在のスクロール量
+    let windowHeight; //ブラウザ内の表示領域の高さ
+    let scrollInView; //スクロールエフェクトのキーとなる値
+    scrollEffect = () => {
+      scrollPosition = pageYOffset;
+      windowHeight = d.documentElement.clientHeight;
+      for (let i = 0; i < effectElement.length; i++) {
+        //ビューポートから各要素のtopまでの値　- ブラウザの表示領域 + スクロール量
+        scrollInView = effectElement[i].getBoundingClientRect().top - windowHeight + scrollPosition;
+        //各要素が表示領域に入った時クラスを付与
+        if (scrollPosition > scrollInView) {
+          effectElement[i].classList.add('add-inView');
+        }
+      }
+      if (effectElement[effectElement.length - 1].classList.contains('add-inView')) {
+        //イベント解除
+        w.removeEventListener('scroll',scrollEffect);
+      }
+    }
+    w.addEventListener('scroll',scrollEffect);
   });
 
   /*---------------------------------------------
     動画 ポップアップ
   -----------------------------------------------*/
-  const popupTrigger = document.querySelectorAll('.js-popupTrigger');
-  const videoPopup = document.querySelectorAll('.js-videoPopup');
-  const video = document.querySelectorAll('.js-videoPopup video');
-  const popupOverlay = document.querySelectorAll('.js-videoPopupOverlay');
-  for (let k = 0; k < popupTrigger.length; k++) {
-    // ポップアップ 開始
-    popupTrigger[k].addEventListener('click', () => {
-      videoPopup[k].classList.add('add-popup');
-      setTimeout(function(){
-        popupOverlay[k].classList.add('add-popup');
-        video[k].classList.add('add-popup');
-      }, 10);
-      // モバイル端末でのタッチスクロール操作の制御
-      document.addEventListener('touchmove', noScroll, { passive: false });
-      // PCでマウスホイールでのスクロール操作の制御
-      document.addEventListener('mousewheel', noScroll, { passive: false });
-      // ios端末でのタッチスクロール操作の制御 & 全てのスクロール操作の制御（ウィンドウ表示域で固定）
-      document.querySelector('body').classList.add('add-ScrollProhibited');
-    });
-
-    // ポップアップ 終了
-    popupOverlay[k].addEventListener('click', () => {
-      popupClose();
-    });
-    document.querySelectorAll('.js-videoClose')[k].addEventListener('click', () => {
-      popupClose();
-    });
-
-  }
-  // ポップアップ 終了 関数
-  popupClose = () => {
-    for (let l = 0; l < popupTrigger.length; l++) {
-      popupOverlay[l].classList.remove('add-popup');
-      video[l].classList.remove('add-popup');
-      setTimeout(function () {
-        videoPopup[l].classList.remove('add-popup');
-      }, 300);
-      video[l].pause();
-    }
-    //スクロール操作禁止解除
-    document.removeEventListener('touchmove', noScroll, { passive: false });
-    document.removeEventListener('mousewheel', noScroll, { passive: false });
-    document.querySelector('body').classList.remove('add-ScrollProhibited');
-  }
+  const popupTrigger = d.querySelectorAll('.js-popupTrigger');
+  const videoPopup = d.querySelectorAll('.js-videoPopup');
+  const video = d.querySelectorAll('.js-videoPopup video');
+  const popupOverlay = d.querySelectorAll('.js-videoPopupOverlay');
 
   // スクロール 禁止関数
   noScroll = () => {
     event.preventDefault();
 　}
 
-  /*---------------------------------------------
-    スクロールエフェクト
-  -----------------------------------------------*/
-  const effectElement = document.querySelectorAll('.js-inView');
-  let scrollPosition; //現在のスクロール量
-  let windowHeight; //ブラウザ内の表示領域の高さ
-  let scrollInView; //スクロールエフェクトのキーとなる値
-  window.addEventListener('scroll', () => {
-    scrollPosition = window.pageYOffset;
-    windowHeight = document.documentElement.clientHeight;
-    for (let m = 0; m < effectElement.length; m++) {
-      //ビューポートから各要素のtopまでの値　- ブラウザの表示領域 + スクロール量
-      scrollInView = effectElement[m].getBoundingClientRect().top - windowHeight + scrollPosition;
-      //各要素が表示領域に入った時クラスを付与
-      if (scrollPosition > scrollInView) {
-        effectElement[m].classList.add('add-inView');
-      }
+  // ポップアップ 終了 関数
+  popupClose = () => {
+    for (let i = 0; i < popupTrigger.length; i++) {
+      popupOverlay[i].classList.remove('add-popup');
+      video[i].classList.remove('add-popup');
+      setTimeout(() => {
+        videoPopup[i].classList.remove('add-popup');
+      }, 300);
+      video[i].pause();
     }
-  });
+    //スクロール操作禁止解除
+    d.removeEventListener('touchmove', noScroll);
+    d.removeEventListener('mousewheel', noScroll);
+    d.body.classList.remove('add-ScrollProhibited');
+  }
 
-});
+  for (let i = 0; i < popupTrigger.length; i++) {
+    // ポップアップ 開始 関数
+    popupTrigger[i].addEventListener('click', () => {
+      videoPopup[i].classList.add('add-popup');
+      setTimeout(() => {
+        popupOverlay[i].classList.add('add-popup');
+        video[i].classList.add('add-popup');
+      }, 10);
+      // モバイル端末でのタッチスクロール操作の制御
+      d.addEventListener('touchmove', noScroll, { passive: false });
+      // PCでマウスホイールでのスクロール操作の制御
+      d.addEventListener('mousewheel', noScroll, { passive: false });
+      // ios端末でのタッチスクロール操作の制御 & 全てのスクロール操作の制御（ウィンドウ表示域で固定）
+      d.body.classList.add('add-ScrollProhibited');
+    });
+    // ポップアップ 終了
+    popupOverlay[i].addEventListener('click', popupClose);
+    d.querySelectorAll('.js-videoClose')[i].addEventListener('click', popupClose);
+  }
+
+})(document,window);
