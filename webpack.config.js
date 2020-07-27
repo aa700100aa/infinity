dfda768140275b82afa70f706b58deeba269c325
aa700100aa <aa700100aa@gmail.com>
2020-07-27 22:16:39 +0900

    feature/dummybranch 指摘対応2 ファイル再作成

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+// For instructions about this file refer to
+// webpack and webpack-hot-middleware documentation
+var webpack = require('webpack');
+var path = require('path');
+var glob = require("glob");
+
+// filenameを取得するメソッドを追記
+// 後ほど使用
+String.prototype.filename = function(){
+  return this.match(".+/(.+?)([\?#;].*)?$")[1];
+}
+
+// ./js/配下のindex.jsのファイルをpathも含めて取得する
+var targets = glob.sync(`./js/*.js`);
+
+// entryに入れるhash
+var entries = {};
+
+// pathも含めたfilenameからpathとfilenameでhashを作る
+targets.forEach(value => {
+  var re = new RegExp(`./js/`);
+  var key = value.replace(re, '');
+
+  entries[key] = value;
+});
+
+module.exports = {
+  // モード値を production に設定すると最適化された状態で、
+  // development に設定するとソースマップ有効でJSファイルが出力される
+  mode: "production",
+  // 入力ファイル エントリーポイント
+  entry: entries, //作成したhashをset
+  // 出力ファイル
+  output: {
+    filename: "[name]"
+  },
+
+  module: {
+    rules: [
+      {
+        // 拡張子 .js の場合
+        test: /\.js$/,
+        use: [
+          {
+            // Babel を利用する
+            loader: 'babel-loader',
+            // Babel のオプションを指定する
+            options: {
+              presets: [
+                // プリセットを指定することで、ES2020 を ES5 に変換
+                '@babel/preset-env',
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    })
+  ]
+};
